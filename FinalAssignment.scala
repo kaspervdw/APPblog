bfa1077c7d059e526614beeabdf612007e265354
Kasper van de Wetering <K.vandeWetering@student.han.nl>
2020-06-11 12:41:39 +0200

    finalised assignment

--- FinalAssignment.scala
+++ FinalAssignment.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable.ListBuffer
 import sys.process._
 
 //val repo = getRepo
@@ -31,11 +32,33 @@ def getLocalCommitHash(): String = {
   scala.io.Source.fromFile(".git/refs/heads/master").getLines().next()
 }
 
-def getHashesFromRepo(): String = {
-  scala.io.Source.fromURL("https://github.com/" + getRepoLink() + "/commits/master", "UTF-8").getLines().filter(line => line.matches(".*data-url=\"/" + getRepoLink() + "/commit.*")).next.replaceFirst(".*commit/", "").replaceFirst("/_render.*", "")
+def getHashesFromRepo(): List[String] = {
+  val page = scala.io.Source.fromURL("https://github.com/" + getRepoLink() + "/commits/master", "UTF-8").getLines().filter(line => line.matches(".*data-url=\"/" + getRepoLink() + "/commit.*"))
+  val listOfHashes = ListBuffer[String]()
+
+  while (page.hasNext) {
+    listOfHashes.addOne(page.next().replaceFirst(".*commit/", "").replaceFirst("/_render.*", ""))
+  }
+
+  listOfHashes.toList
 }
 
-println(getHashesFromRepo())
+println("Checking if your commit is already on the remote...")
+if (getHashesFromRepo().contains(getLocalCommitHash())) {
+  println("Your commit has already been pushed!")
+  println("Checking if you're up to date with the remote...")
+  if (!getHashesFromRepo().head.equals(getLocalCommitHash())) {
+    println("The remote repository is ahead of your local repository. Pulling updates...")
+    if ("git pull".! != 0) {
+      println("There was a problem while pulling the changes...")
+    } else {
+      "Pulled remote changes successfully..."
+    }
+  }
+  println("Seems like you're fully up to date with the remote repository!")
+} else {
+  println("Your local commit hasn't been pushed to the remote repository yet! Please do so or discard your local changes before executing this program again...")
+}
 
 
 
