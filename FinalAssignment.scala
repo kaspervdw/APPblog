bcf0b72a9abc4000f05f8085fc635c823b07fdad
Kaspervdw <Kaspervdw@live.nl>
2020-06-11 13:20:53 +0200

    final commit

--- FinalAssignment.scala
+++ FinalAssignment.scala
@@ -1,35 +1,18 @@
 import scala.collection.mutable.ListBuffer
 import sys.process._
 
-//val repo = getRepo
-//val latestHash = retrieveCommitHash
-//val currentHash = scala.io.Source.fromFile(".git/refs/heads/master").getLines.next
-//
-//if (!currentHash.equals(latestHash)) {
-//  println("It seems the remote repository is ahead of the local repository. Pulling to retrieve the changes.\n----------")
-//  if ("git pull".! != 0) {
-//    println("----------\nThere was a problem while updating the repository.")
-//  }
-//}
-//
-//def getRepo(): String = {
-//  scala.io.Source.fromFile(".git/config").getLines.filter(line => line.matches("^\turl =.*")).mkString.stripPrefix("\turl = git@github.com:")
-//}
-//
-//def retrieveCommitHash(): String = {
-//  val doc = scala.io.Source.fromURL("https://github.com/" + repo + "/commits/master", "UTF-8")
-//  val returnVal = doc.getLines.filter(line => line.matches(".*class=\"message\".*")).next.replaceFirst("", "").replaceFirst("^.*href=\".*/", "").replaceFirst("\".*$", "")
-//  doc.close
-//  returnVal
-//}
-//
-
 def getRepoLink(): String = {
-  scala.io.Source.fromFile(".git/config").getLines().filter(line => line.matches("^\turl =.*")).mkString.stripPrefix("\turl = git@github.com:").stripSuffix(".git")
+  val doc = scala.io.Source.fromFile(".git/config")
+  val repoLink = doc.getLines().filter(line => line.matches("^\turl =.*")).mkString.stripPrefix("\turl = git@github.com:").stripSuffix(".git")
+  doc.close()
+  repoLink
 }
 
 def getLocalCommitHash(): String = {
-  scala.io.Source.fromFile(".git/refs/heads/master").getLines().next()
+  val doc = scala.io.Source.fromFile(".git/refs/heads/master")
+  val commitHash = doc.getLines().next
+  doc.close
+  commitHash
 }
 
 def getHashesFromRepo(): List[String] = {
@@ -52,10 +35,11 @@ if (getHashesFromRepo().contains(getLocalCommitHash())) {
     if ("git pull".! != 0) {
       println("There was a problem while pulling the changes...")
     } else {
-      "Pulled remote changes successfully..."
+      println("Pulled remote changes successfully...")
     }
+  } else {
+    println("Seems like you're fully up to date with the remote repository!")
   }
-  println("Seems like you're fully up to date with the remote repository!")
 } else {
   println("Your local commit hasn't been pushed to the remote repository yet! Please do so or discard your local changes before executing this program again...")
 }
